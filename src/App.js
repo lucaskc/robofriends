de37b3cd85313cdccf841baafd32b9067109ec56
Lucas Kassouf Crocomo <lucas.crocomo@gmail.com>
2019-05-25 20:58:52 -0300

    new updates to scrolling bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
@@ -30,13 +31,19 @@ class App extends Component {
 			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		});
 		
-		return (
-			<div className='tc'>
-				<h1 className='f1'>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange} />
-				<CardList robots={filteredRobots} />
-			</div>
-		);
+		if (this.state.robots.length === 0) {
+			return <h1 className='tc'>Loading</h1>
+		} else {
+			return (
+				<div className='tc'>
+					<h1 className='f1'>RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange} />
+					<Scroll>
+						<CardList robots={filteredRobots} />
+					</Scroll>
+				</div>
+			);
+		}
 	}
 }
 
