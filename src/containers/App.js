723e6c906c986cd69fc0e341819030a48fbcab93
Lucas <lucas.crocomo@usp.br>
2019-06-05 14:45:40 -0300

    replaced react searchField state to redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -6,13 +7,27 @@ import ErrorBoundary from '../components/ErrorBoundary';
 import BouncingLoader from '../components/BouncingLoader';
 import './App.css';
 
+import { setSearchField } from '../actions';
+
+
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchField
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
+
 class App extends Component {
 
 	constructor() {
 		super();
 		this.state = {
-			robots: [],
-			searchfield: ''
+			robots: []
 		};
 	}
 
@@ -23,14 +38,11 @@ class App extends Component {
 
 	}
 
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value });
-	}
-
 	render() {
-		const { robots, searchfield } = this.state;
+		const { robots } = this.state;
+		const { searchField, onSearchChange } = this.props;
 		const filteredRobots = robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		});
 		
 		if (!robots.length) {
@@ -39,7 +51,7 @@ class App extends Component {
 			return (
 				<div className='tc'>
 					<h1 className='f1'>RoboFriends</h1>
-					<SearchBox searchChange={this.onSearchChange} />
+					<SearchBox searchChange={onSearchChange} />
 					<Scroll>
 						<ErrorBoundary>
 							<CardList robots={filteredRobots} />
@@ -51,4 +63,4 @@ class App extends Component {
 	}
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
